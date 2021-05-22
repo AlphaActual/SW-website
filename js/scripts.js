f58b78c6a8c513e9aa8732c15802a1fb9c07f009
Tin Pritišanac <75732610+AlphaActual@users.noreply.github.com>
2021-05-22 18:15:13 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,2 @@
-alert('Disclaimer! This Web page is not intended for any commercial or any other use other than as a html/css practice project!');
+//alert('Disclaimer! This Web page is not intended for any commercial or any other use other than as a html/css practice project!');
+
